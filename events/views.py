50ff6d875214b008abf1f92d4946db0f8e12f294
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-11-05 23:23:21 +0300

    Add comment for CompetitorsListVIew

--- events/views.py
+++ events/views.py
@@ -4,5 +4,8 @@ from events import serializers, models
 
 
 class CompetitorsListView(generics.ListAPIView):
+    """
+    Returns a list of available competitors
+    """
     queryset = models.CompetitorType.objects.all()
     serializer_class = serializers.CompetitorTypeSerializer
