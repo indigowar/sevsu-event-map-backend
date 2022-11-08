e7bacc81421d5afb37248ae0c128e1267858de68
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-11-08 16:25:47 +0000

    Move all views of v1 API to separated folder.

---
+++ events/api/v1/urls.py
@@ -0,0 +1,35 @@
+from django.urls import path
+
+from . import foundings, organizer, competitor, event, subjects
+
+urlpatterns = [
+    # Organizer levels
+    path('organizer_levels/', organizer.LevelsListAPIView.as_view()),
+
+    # Organizers
+    path('organizer/', organizer.ListCreateAPIView.as_view()),
+    path('organizer/<int:pk>', organizer.RetrieveUpdateDestroyAPIView.as_view()),
+
+    # Competitor Types
+    path('competitors/', competitor.ListView.as_view()),
+
+    # Founding Types
+    path('founding_types/', foundings.TypeListView.as_view()),
+
+    # Founding Range
+    path('founding_range/', foundings.FoundingRangeListCreateAPIView.as_view()),
+    path('founding_range/<int:pk>', foundings.FoundingRangeRetrieveUpdateDestroyView.as_view()),
+
+    path('co_founding_range/', foundings.CoFoundingCreateAPIView.as_view()),
+    path('co_founding_range/<int:pk>', foundings.CoFoundingRangeRetrieveUpdateDestroyAPIView.as_view()),
+
+    # Event
+    path('event/', event.ListCreateAPIView.as_view()),
+    path('event/<int:pk>/', event.RetrieveUpdateDestroyAPIView.as_view()),
+    path('event/<int:pk>/minimal', event.MinimalRetrieveAPIView.as_view()),
+
+    # Subjects
+    path('subject/', subjects.ListCreateAPIView.as_view()),
+    path('subject/event/<int:event>/', subjects.ListByEventAPIView.as_view()),
+    path('subject/id/<int:pk>/', subjects.RetrieveUpdateDestroyAPIView.as_view())
+]
--- events/urls.py
+++ events/urls.py
@@ -1,35 +1,5 @@
-from django.urls import path
-
-from .views import event, organizer, competitor, foundings, subjects
+from django.urls import path, include
 
 urlpatterns = [
-    # Organizer levels
-    path('organizer_levels/', organizer.LevelsListAPIView.as_view()),
-
-    # Organizers
-    path('organizer/', organizer.ListCreateAPIView.as_view()),
-    path('organizer/<int:pk>', organizer.RetrieveUpdateDestroyAPIView.as_view()),
-
-    # Competitor Types
-    path('competitors/', competitor.ListView.as_view()),
-
-    # Founding Types
-    path('founding_types/', foundings.TypeListView.as_view()),
-
-    # Founding Range
-    path('founding_range/', foundings.FoundingRangeListCreateAPIView.as_view()),
-    path('founding_range/<int:pk>', foundings.FoundingRangeRetrieveUpdateDestroyView.as_view()),
-
-    path('co_founding_range/', foundings.CoFoundingCreateAPIView.as_view()),
-    path('co_founding_range/<int:pk>', foundings.CoFoundingRangeRetrieveUpdateDestroyAPIView.as_view()),
-
-    # Event
-    path('event/', event.ListCreateAPIView.as_view()),
-    path('event/<int:pk>/', event.RetrieveUpdateDestroyAPIView.as_view()),
-    path('event/<int:pk>/minimal', event.MinimalRetrieveAPIView.as_view()),
-
-    # Subjects
-    path('subject/', subjects.ListCreateAPIView.as_view()),
-    path('subject/event/<int:event>/', subjects.ListByEventAPIView.as_view()),
-    path('subject/id/<int:pk>/', subjects.RetrieveUpdateDestroyAPIView.as_view())
+    path('/v1/', include('events.api.v1.urls')),
 ]
